5e40845552458c846baf447d7def215e0a275e77
S.Sarayoot <Admin@Apple-Macintosh-2.local>
2015-06-29 03:27:38 +0700

    set angularjs and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,19 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+        .scripts([
+            // Combine all js files into one.
+            // Path is relative to resource/js folder.
+            '../../bower_components/jquery/dist/jquery.min.js',
+            '../../bower_components/velocity/velocity.min.js',
+            '../../bower_components/moment/min/moment-with-locales.min.js',
+            '../../bower_components/angular/angular.min.js',
+            '../../bower_components/lumx/dist/lumx.min.js',
+        ], 'public/js/app.js')
+        // Since css file will be place into public/css folder,
+        // we need to copy font files too
+        .copy('bower_components/mdi/fonts', 'public/fonts');
 });
+
+
