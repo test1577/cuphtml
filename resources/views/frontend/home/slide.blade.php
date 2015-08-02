ee2adc52ac64f13516a0878c9b53954165b4ed8d
S.Sarayoot <sarayoot@3dsinteractive.com>
2015-08-02 11:18:28 +0700

    update slide css

--- resources/views/frontend/home/slide.blade.php
+++ resources/views/frontend/home/slide.blade.php
@@ -4,10 +4,14 @@
 
     <!--Slide-->
     <div class="col-md-12 col-sm-12 col-xs-12">
-      <div id="slide-cuphtml" class="owl-carousel owl-theme">
-        <div class="item"><img src="{{ $global['baseUrl'] }}assets/plugins/owl-carousel/images/fullimage1.jpg" alt="The Last of us"></div>
-        <div class="item"><img src="{{ $global['baseUrl'] }}assets/plugins/owl-carousel/images/fullimage2.jpg" alt="GTA V"></div>
-        <div class="item"><img src="{{ $global['baseUrl'] }}assets/plugins/owl-carousel/images/fullimage3.jpg" alt="Mirror Edge"></div>
+      <div class="tab-left">
+        <div class="section">
+          <div id="slide-cuphtml" class="owl-carousel owl-theme">
+            <div class="item"><img src="{{ $global['baseUrl'] }}assets/plugins/owl-carousel/images/fullimage1.jpg" alt="The Last of us"></div>
+            <div class="item"><img src="{{ $global['baseUrl'] }}assets/plugins/owl-carousel/images/fullimage2.jpg" alt="GTA V"></div>
+            <div class="item"><img src="{{ $global['baseUrl'] }}assets/plugins/owl-carousel/images/fullimage3.jpg" alt="Mirror Edge"></div>
+          </div>
+        </div>
       </div>
     </div>
 
