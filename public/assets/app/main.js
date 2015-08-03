df749ac09c925178a7b352028b30392dbce8b788
S.Sarayoot <sarayoot@3dsinteractive.com>
2015-08-03 21:37:15 +0700

    update main js

--- public/assets/app/main.js
+++ public/assets/app/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-  var cuphtml = (function () {	
+  var cuphtml = (function () {
     var option = {
-      'init': function () {          
+      'init': function () {
         option.liberies.bootstrap.dropdown();
         option.liberies.wow();
         option.liberies.owlCarousel();
+        option.button.btnScrollTop();
+        option.windowEvent.scroll.on();
+      },
+      
+      'object': {
+        'scrollPositionCurrent': 0
       },
       
       'liberies': {
@@ -17,17 +23,17 @@ $(document).ready(function () {
         'wow': function () {
           var wow = new WOW({
             offset: 75, // distance to the element when triggering the animation (default is 0)
-            mobile: false, // trigger animations on mobile devices (default is true)
+            mobile: false // trigger animations on mobile devices (default is true)
           });
           wow.init();
         },
         
         'owlCarousel': function () {
-          $("#slide-cuphtml").owlCarousel({
-              navigation : false, // Show next and prev buttons
-              slideSpeed : 300,
-              paginationSpeed : 400,
-              singleItem:true
+          $("#slide-cuphtml, #slide-cuphtml-banner").owlCarousel({
+            navigation: false, // Show next and prev buttons
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            singleItem: true
               // "singleItem:true" is a shortcut for:
               // items : 1, 
               // itemsDesktop : false,
@@ -36,12 +42,75 @@ $(document).ready(function () {
               // itemsMobile : false
           });
         }
+
+      },
+      'button': {
+        btnScrollTop: function () {
+          $btnScrollTop = '<span class="fix-btn-scroll' +
+            ' glyphicon glyphicon-eject"' +
+            ' aria-hidden="true"></span>';
+          $body = $('body');
+          $body.append($btnScrollTop);
+          var $elBtn = $body.find('.fix-btn-scroll');
+          $elBtn.on("click", function () {
+            $('html,body').animate({scrollTop: (0)}, 1000);
+          });
+        },
         
+        'showBtnToTop': function (scroll) {
+          $body = $('body');
+          var $elBtn = $body.find('.fix-btn-scroll');
+          if (scroll > 300) {
+            $elBtn.fadeIn();
+          } else {
+            $elBtn.fadeOut();
+          }
+        }
+        
+      },
+      'windowEvent': {
+        'scroll': {
+          'on': function () {
+            $(window).on("scroll", function (e) {
+              $scroll = $(window).scrollTop();
+              option.button.showBtnToTop($scroll);
+              option.windowEvent.scroll.checkValueScroll($scroll);
+            });
+          },
+          
+          'setValueScroll': function (scroll) {
+            option.object.scrollPositionCurrent = scroll;
+          },
+          
+          'checkValueScroll': function (scroll) {
+            var scrollValueOld = option.object.scrollPositionCurrent;
+            if (scroll > 300 && scroll > scrollValueOld) {
+              option.windowEvent.click.on();
+              option.windowEvent.scroll.stopScroll();
+              option.windowEvent.scroll.setValueScroll($scroll);
+            } else {
+              option.windowEvent.scroll.setValueScroll($scroll);
+            }
+          },
+          
+          'stopScroll': function () {
+            $('html,body').stop();
+          }
+          
+        },
+        
+        'click' : {
+          'on': function () {
+            $(window).on("click", function (e) {
+            });
+          }
+          
+        }
       }
     };
-	option.init();
-	return option;
-})(jQuery);
+    option.init();
+    return option;
+  })(jQuery);
 
 
 
